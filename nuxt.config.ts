f355d9ea1cd26b9669da67a63019941f00bd0bd5
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-02-25 18:03:39 +0700

    Initial routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
   nitro: {
     preset: "static",
   },
+
+  modules: ["@nuxt/ui"],
 });
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-screen h-screen flex bg-[url(/img/tarot-bg.jpg)] justify-center items-center">
+    <div class="flex h-[50%] w-full gap-4 justify-center items-center">
+      <NuxtLink to="/foolvpn" class="h-full">
+        <img
+          src="/img/the-fool.png"
+          class="h-full grayscale hover:grayscale-0 transition-all duration-200 hover:scale-105"
+          alt="Tarot Card"
+        />
+      </NuxtLink>
+      <NuxtLink to="/nautica" class="h-full">
+        <img
+          src="/img/the-star.png"
+          class="h-full grayscale hover:grayscale-0 transition-all duration-200 hover:scale-105"
+          alt="Tarot Card"
+        />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
