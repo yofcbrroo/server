febe33d1736eaa013c7502b707bfe4516c3786aa
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-03-29 10:38:51 +0700

    ci: revert using pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   nitro: {
-    preset: "static",
+    preset: "cloudflare_pages",
   },
   tailwindcss: {
     exposeConfig: true,
   },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxt/icon",
-    "@pinia/nuxt",
-    "@nuxthub/core",
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@pinia/nuxt", "@nuxthub/core"],
 });
