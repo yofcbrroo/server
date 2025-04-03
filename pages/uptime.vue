2966171a4ec295d3439ab99cde46f594d4b91545
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-04-03 18:05:01 +0700

    page: redesign

--- pages/uptime.vue
+++ pages/uptime.vue
@@ -48,7 +48,7 @@ async function getServerPing(server: string) {
     if (serverList.value[i].url == server) {
       serverList.value[i].ping.push(pingData);
 
-      if (serverList.value[i].ping.length > 58) {
+      if (serverList.value[i].ping.length > 22) {
         serverList.value[i].ping.shift();
       }
     }
@@ -95,93 +95,109 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center overflow-hidden">
     <div class="w-[80%] lg:w-[50%] flex justify-around flex-wrap gap-2 p-2">
       <Card v-for="server in serverList">
-        <div
-          class="mx-5 my-2 grid grid-rows-3 gap-3 lg:grid-rows-none lg:grid-cols-3 font-bold"
-          v-if="server.info.country && server.status.mem"
-        >
-          <div class="flex items-center justify-center lg:justify-start font-bold gap-3">
-            <span class="text-3xl hidden lg:block">
-              {{ getFlagEmoji(server.info.country) }}
-            </span>
-            <div class="flex flex-col items-center lg:items-start">
-              <span class="text-sm">
-                {{ server.info.country_name }}
+        <div class="mx-5 my-2 grid grid-rows-3 gap-3 lg:grid-rows-none lg:grid-cols-3 font-bold">
+          <div class="w-full h-full">
+            <div v-if="server.info.country" class="flex items-center justify-center lg:justify-start font-bold gap-3">
+              <span class="text-3xl hidden lg:block">
+                {{ getFlagEmoji(server.info.country) }}
               </span>
-              <span class="text-xl">
-                {{ server.info.org }}
-              </span>
-              <span class="text-xs font-normal italic">
-                {{ server.status.host.os }}
-                {{ server.status.host.platformFamily }}
-                {{ server.status.host.platformVersion }}
-              </span>
+              <div class="flex flex-col items-center lg:items-start">
+                <span class="text-sm">
+                  {{ server.info.country_name }}
+                </span>
+                <span class="text-xl">
+                  {{ server.info.org }}
+                </span>
+                <span class="text-xs font-normal italic">
+                  {{ server.status.host.os }}
+                  {{ server.status.host.platformFamily }}
+                  {{ server.status.host.platformVersion }}
+                </span>
+              </div>
+            </div>
+            <div v-else class="w-full h-16 skeleton bg-primary flex justify-center items-center text-neutral-content">
+              Fetching {{ server.url }}
             </div>
           </div>
           <div class="row-span-2 lg:col-span-2 flex flex-col justify-center items-center gap-5">
-            <div class="flex gap-5">
-              <div class="flex flex-col">
-                <div>Ping</div>
-                <div class="text-xs">{{ server.ping[server.ping.length - 1].delay }} ms</div>
-                <div>
-                  <progress
-                    class="progress progress-secondary w-12"
-                    :value="server.ping.filter((data: any) => data.delay > 0 ).length"
-                    :max="server.ping.length"
-                  ></progress>
+            <div v-if="server.status.cpu" class="flex flex-col lg:flex-row lg:gap-5">
+              <div class="flex gap-5">
+                <div class="flex flex-col">
+                  <div>CPU</div>
+                  <div v-if="server.status.cpu">
+                    <div class="text-xs">{{ parseInt(server.status.cpu[0]).toFixed() }} %</div>
+                    <div>
+                      <progress
+                        class="progress progress-secondary w-12"
+                        :value="parseInt(server.status.cpu[0]).toFixed()"
+                        max="100"
+                      ></progress>
+                    </div>
+                  </div>
+                </div>
+                <div class="flex flex-col">
+                  <div>RAM</div>
+                  <div v-if="server.status.mem">
+                    <div class="text-xs">{{ prettyBytes(server.status.mem.used) }}</div>
+                    <div>
+                      <progress
+                        class="progress progress-secondary w-12"
+                        :value="server.status.mem.used"
+                        :max="server.status.mem.total"
+                      ></progress>
+                    </div>
+                  </div>
+                </div>
+                <div class="flex flex-col">
+                  <div>Upload</div>
+                  <div v-if="server.speed.upload && server.status.nic">
+                    <div class="text-xs">
+                      {{ prettyBytes(server.speed.upload) }}
+                    </div>
+                    <div class="text-sm">
+                      {{ prettyBytes(server.status.nic[0].bytesSent) }}
+                    </div>
+                  </div>
+                </div>
+                <div class="flex flex-col">
+                  <div>Download</div>
+                  <div v-if="server.speed.download && server.status.nic">
+                    <div class="text-xs">
+                      {{ prettyBytes(server.speed.download) }}
+                    </div>
+                    <div class="text-sm">
+                      {{ prettyBytes(server.status.nic[0].bytesRecv) }}
+                    </div>
+                  </div>
+                </div>
+                <div class="flex flex-col">
+                  <div>Ping</div>
+                  <div v-if="server.ping.length > 0">
+                    <div class="text-xs">{{ server.ping[server.ping.length - 1].delay }} ms</div>
+                    <div>
+                      <progress
+                        class="progress progress-secondary w-12"
+                        :value="server.ping.filter((data: any) => data.delay > 0 ).length"
+                        :max="server.ping.length"
+                      ></progress>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div class="flex flex-col">
-                <div>CPU</div>
-                <div class="text-xs">{{ parseInt(server.status.cpu[0]).toFixed() }} %</div>
-                <div>
-                  <progress
-                    class="progress progress-secondary w-12"
-                    :value="parseInt(server.status.cpu[0]).toFixed()"
-                    max="100"
-                  ></progress>
-                </div>
-              </div>
-              <div class="flex flex-col">
-                <div>RAM</div>
-                <div class="text-xs">{{ prettyBytes(server.status.mem.used) }}</div>
-                <div>
-                  <progress
-                    class="progress progress-secondary w-12"
-                    :value="server.status.mem.used"
-                    :max="server.status.mem.total"
-                  ></progress>
-                </div>
-              </div>
-              <div class="flex flex-col">
-                <div>Upload</div>
-                <div class="text-xs">
-                  {{ prettyBytes(server.speed.upload) }}
-                </div>
-                <div>
-                  {{ prettyBytes(server.status.nic[0].bytesSent) }}
-                </div>
-              </div>
-              <div class="flex flex-col">
-                <div>Download</div>
-                <div class="text-xs">
-                  {{ prettyBytes(server.speed.download) }}
-                </div>
-                <div>
-                  {{ prettyBytes(server.status.nic[0].bytesRecv) }}
+              <div v-if="server.ping.length > 0" class="flex flex-wrap gap-1 lg:w-48 h-full justify-end px-1">
+                <div
+                  v-for="pingData in server.ping"
+                  class="tooltip"
+                  :data-tip="`${pingData.delay} ms (${pingData.date})`"
+                >
+                  <div class="badge badge-xs" :class="pingData.delay > 0 ? 'badge-secondary' : 'badge-accent'"></div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div v-else class="h-24 w-full bg-primary skeleton flex justify-center items-center text-gray-500">
-          {{ server.url }}
-        </div>
-        <div v-if="server.ping.length > 0" class="flex gap-1 w-full justify-end pb-1 px-1">
-          <div v-for="pingData in server.ping" class="tooltip" :data-tip="`${pingData.delay} ms (${pingData.date})`">
-            <div class="badge badge-xs" :class="pingData.delay > 0 ? 'badge-success' : 'badge-error'"></div>
+            <div v-else class="skeleton w-full h-16 bg-primary"></div>
           </div>
         </div>
       </Card>
