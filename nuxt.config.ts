3c7bbc4c13181e18e81f21a944229c8c157992e4
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-05-12 18:24:27 +0700

    remove nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  nitro: {
-    preset: "netlify_edge",
-  },
   tailwindcss: {
     exposeConfig: true,
   },
