e6d48ffada8f74b29ca24a3b7ee971d197ab2cd1
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-05-12 18:20:52 +0700

    use netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   nitro: {
-    preset: "cloudflare_pages",
+    preset: "netlify_edge",
   },
   tailwindcss: {
     exposeConfig: true,
