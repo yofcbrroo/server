9513ebb92d2075f94a8c835ba938cbe6126aba1d
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-09-11 22:11:30 +0700

    bun

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  nitro: {
+    preset: "cloudflare-pages",
+  },
   devtools: { enabled: true },
   tailwindcss: {
     exposeConfig: true,
