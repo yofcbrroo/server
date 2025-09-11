e0eb1e2be23e13d33b93e048c301da454669d89e
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-09-11 22:26:57 +0700

    Set as workers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  nitro: {
-    preset: "cloudflare-pages",
-  },
   devtools: { enabled: true },
   tailwindcss: {
     exposeConfig: true,
